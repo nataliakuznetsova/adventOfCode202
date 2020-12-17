1457b96f5001ce49f1929d814014f14e4187648a
Kuznetsova <n.kuznetsova@catawiki.nl>
2020-12-17 18:29:39 +0100

    Assignment 3

---
+++ app/src/test/java/com/fun/adventofcode2020/Assignment3Test.kt
@@ -0,0 +1,15 @@
+package com.`fun`.adventofcode2020
+
+import junit.framework.TestCase
+import org.junit.Test
+
+class Assignment3Test : TestCase() {
+
+    @Test
+    internal fun test1() {
+        val assignment3 = Assignment3()
+
+        assignment3.count()
+    }
+
+}
