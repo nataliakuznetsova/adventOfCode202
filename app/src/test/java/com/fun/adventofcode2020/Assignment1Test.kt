652b4e1493d246d8b92f966a5e2865ef7c009195
Kuznetsova <n.kuznetsova@catawiki.nl>
2020-12-17 18:29:02 +0100

    Assignment 1

---
+++ app/src/test/java/com/fun/adventofcode2020/Assignment1Test.kt
@@ -0,0 +1,12 @@
+package com.`fun`.adventofcode2020
+
+import junit.framework.TestCase
+
+class Assignment1Test : TestCase() {
+
+    fun test1() {
+        val a = Assignment1()
+
+        a.find()
+    }
+}
